f5617e277727aa89024168645c542b97c643adc0
t.mittlener <t.mittlener@gmail.com>
2017-04-05 14:06:30 +0200

    feat: add lodash

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import map from 'lodash/map'
 
 
 class TestPage extends React.Component {
   render() {
+    let TestArray =[1,2, 3];
     return (
       <div className="index">
         <div className="notice"><h3>Test Page with:</h3>
@@ -11,6 +13,9 @@ class TestPage extends React.Component {
             <li>SCSS</li>
             <li>Susy</li>
             <li>Modular Scale</li>
+            <li>Lodash - test:
+              {map(TestArray, (no, index) => <span key={index}> ({no}) </span>)}
+            </li>
           </ul>
         </div>
       </div>
