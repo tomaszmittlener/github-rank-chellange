db1775642a33df000b1bb3aacd5234ec75c48956
t.mittlener <t.mittlener@gmail.com>
2017-04-08 17:43:33 +0200

    feat: add user repos list

---
+++ src/components/UserReposList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import List from './List';
+
+
+class UserReposList extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+
+    return (
+      <List className="list--UserReposList">
+        User Repos List
+
+
+
+      </List>
+    );
+  }
+}
+
+UserReposList.defaultProps = {
+};
+
+export default UserReposList;
